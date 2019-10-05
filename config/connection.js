80e7035c4901187907325ed7aa1471e0f23e9626
Sara Sha <sarandamatoshi@hotmail.com>
2019-10-05 16:56:55 -0700

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Mysql.2019",
-  database: "burgers_db"
-});
-
+//if we are on Heroku and the JawsDB addon is configured, we will have the environment variable JAWSDB_URL
+//this variable will contain what mysql package needs to connect :)
+require('dotenv').config();
+var mysql = require('mysql');
+var connection;
+if(process.env.JAWSDB_URL) {  
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //otherwise, we're going to use our local connection!  put your local db set stuff here
+  //(and remember our best practice of using the dotenv package and a .env file ;)
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.LOCAL_DB_USER,
+    password: process.env.LOCAL_DB_PASSWORD,
+    database: process.env.LOCAL_DB_DATABASE
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
